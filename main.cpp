802f1c235030c5222fcd89a7fcf4027fa415b712
xhy <hangyu_xia@163.com>
2023-02-01 20:27:38 +0800

    init main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "Emulator.h"
+#include "Log.h"
+#include <string>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv){
+
+    string LOGPATH = "simplenes.log";
+    // std::ofstream logFile(LOGPATH);
+
+    cout << "End." << endl;
+
+    return 0;
+
+}
