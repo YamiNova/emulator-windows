68950047e74231be55483dd782b008c5ff543e85
xhy <hangyu_xia@outlook.com>
2023-03-10 15:29:09 +0800

    modification

--- KeybindingsParser.cpp
+++ KeybindingsParser.cpp
@@ -8,5 +8,22 @@
 #include "Log.h"
 
 namespace sn{
+    inline std::string ltrim(const std::string &str){
+        std::string s(str);
+        s.erase(s.begin(),
+                std::find_if_not<decltype(s.begin()), int(int)>(s.begin(), s.end(), std::isspace));
+        return s;
+    }
+
+    inline std::string rtrim(const std::string &str){
+        std::string s(str);
+        s.erase(std::find_if_not<decltype(s.rbegin()), int(int)>(s.rbegin(), s.rend(), std::isspace).base(),
+                s.end());
+        return s;
+    }
+
+    void parseControllerConf(std::string filepath){
+
+    }   
     
 }
